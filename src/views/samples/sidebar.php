5e56c408ec079d364ca3a2057dba30fc4bd2aafc
L Shaf <shafry@mimicreative.net>
2018-04-19 17:24:49 +0700

    complete sidebar

--- src/views/samples/sidebar.php
+++ src/views/samples/sidebar.php
@@ -1,28 +1,29 @@
 <?php
-use akupeduli\bracket\widgets\Menu;
+use akupeduli\bracket\widgets\Sidebar;
+use rmrevin\yii\ionicon\Ion;
+
 ?>
 <div class="br-sideleft overflow-y-auto">
     <label class="sidebar-label pd-x-10 mg-t-20 op-3">Navigation</label>
-<?=Menu::widget(
+<?=Sidebar::widget(
     [
         "items" => [
-            ["label" => "Home", "url" => [Yii::$app->getHomeUrl()], "icon" => "ion-ios-home-outline"],
-            ["label" => "Test", "url" => ["/test"], "icon" => "ion-ios-home-outline"],
-            ["label" => "Error page", "url" => ["."], "icon" => "ion-ios-home-outline"],
+            ["label" => "Home", "url" => ["site/index"], "icon" => Ion::_IOS_HOME_OUTLINE],
+            ["label" => "Test", "url" => ["/test"], "icon" => Ion::_IOS_HOME_OUTLINE],
+            ["label" => "Error page", "url" => ["."], "icon" => Ion::_IOS_HOME_OUTLINE],
             [
                 "label"  => "Widgets",
-                "icon"   => "ion-ios-home-outline",
-                "url"    => "#",
+                "icon"   => Ion::_IOS_HOME_OUTLINE,
                 'hasSub' => true,
                 "items"  => [
                     ["label" => "Menu", "url" => ["/test"], 'isSub' => true],
                     ["label" => "Panel", "url" => ["."], 'isSub' => true],
                 ],
             ],
-            ["label" => "Error page", "url" => ["."], "icon" => "ion-ios-home-outline"],
+            ["label" => "Error page", "url" => ["."], "icon" => Ion::_IOS_HOME_OUTLINE],
         ],
     ]
 )
 ?>
     <br>
-</div><!-- br-sideleft -->
+</div>
