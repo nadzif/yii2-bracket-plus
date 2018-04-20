a99911c82b7e7281dda129ae1e4252c8a018f15a
L Shaf <shafry@mimicreative.net>
2018-04-20 15:36:12 +0700

    finalize breadcrumbs

--- src/widgets/Breadcrumbs.php
+++ src/widgets/Breadcrumbs.php
@@ -1,6 +1,10 @@
 <?php
 
-namespace akupeduli\material\widgets;
+namespace akupeduli\bracket\widgets;
+
+use yii\base\InvalidConfigException;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
 
 /**
  * @Author: L Shaf
@@ -8,7 +12,31 @@ namespace akupeduli\material\widgets;
  */
 class Breadcrumbs extends \yii\widgets\Breadcrumbs
 {
-    public $tag = "ol";
-    public $itemTemplate = "<li class='breadcrumb-item'>{link}</li>\n";
-    public $activeItemTemplate = "<li class='breadcrumb-item active'>{link}</li>\n";
+    public $tag = "nav";
+    public $options = ['class' => 'breadcrumb pd-0 mg-0 tx-12'];
+    public $itemTemplate = "{link}\n";
+    public $activeItemTemplate = "{link}\n";
+    
+    protected function renderItem($link, $template)
+    {
+        $encodeLabel = ArrayHelper::remove($link, 'encode', $this->encodeLabels);
+        if (array_key_exists('label', $link)) {
+            $label = $encodeLabel ? Html::encode($link['label']) : $link['label'];
+        } else {
+            throw new InvalidConfigException('The "label" element is required for each link.');
+        }
+        if (isset($link['template'])) {
+            $template = $link['template'];
+        }
+        if (isset($link['url'])) {
+            $options = $link;
+            unset($options['template'], $options['label'], $options['url']);
+            Html::addCssClass($options, "breadcrumb-item");
+            $link = Html::a($label, $link['url'], $options);
+        } else {
+            $link = Html::tag("span", $label, ["class" => "breadcrumb-item active"]);
+        }
+    
+        return strtr($template, ['{link}' => $link]);
+    }
 }
